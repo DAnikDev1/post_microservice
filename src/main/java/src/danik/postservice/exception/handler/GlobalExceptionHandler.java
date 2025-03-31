79b0cdcc58a2b2e1e418b07f6980450708f33118
danik <danikdevs@gmail.com>
2025-03-31 17:01:52 +0500

    added FeignException for GlobalExceptionHandler

--- src/main/java/src/danik/postservice/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/src/danik/postservice/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package src.danik.postservice.exception.handler;
 
+import feign.FeignException;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,17 +11,27 @@ import src.danik.postservice.exception.DataValidationException;
 import src.danik.postservice.exception.ExistedEntityException;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse processEntityNotFoundException(EntityNotFoundException e) {
+        log.error("Entity not found exception: {}", e.getMessage());
         return ErrorResponse.builder().message(e.getMessage()).build();
     }
 
     @ExceptionHandler(DataValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse processBadRequestException(DataValidationException e) {
+        log.error("Data validation exception: {}", e.getMessage());
         return ErrorResponse.builder().message(e.getMessage()).build();
+    }
+
+    @ExceptionHandler(FeignException.class)
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    public ErrorResponse processFeignException(FeignException e) {
+        log.error("Feign exception: {}", e.getMessage());
+        return ErrorResponse.builder().message(e.getMessage()).status(400).build();
     }
 
 }
