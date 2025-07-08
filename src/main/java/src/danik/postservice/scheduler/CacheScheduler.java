ed9555d660e6ec9547d58286fde15ea03be66717
danikdev <danikdevs@gmail.com>
2025-07-08 13:07:10 +0500

    cache scheduler

---
+++ src/main/java/src/danik/postservice/scheduler/CacheScheduler.java
@@ -0,0 +1,43 @@
+package src.danik.postservice.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import src.danik.postservice.service.PostService;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CacheScheduler {
+    private final PostService postService;
+    private final CacheManager cacheManager;
+    
+    @Value("${popular.posts.amount:100}")
+    private int popularPostsCount;
+
+    @Scheduled(fixedDelay = 600000)
+    public void generatePopularPostsCache() {
+        log.info("Generating hot cache for popular posts");
+        try {
+            Cache cache = cacheManager.getCache("popularPosts");
+            if (cache != null) {
+                cache.clear();
+            }
+            
+            postService.findPopularPosts(popularPostsCount);
+            
+            log.info("{} posts was cached", popularPostsCount);
+        } catch (Exception e) {
+            log.error("Error to generate hot cache", e);
+        }
+    }
+    @Scheduled(initialDelay = 5000, fixedDelay = Long.MAX_VALUE)
+    public void firstHotCacheInitiate() {
+        log.info("Generating hot cache for start");
+        generatePopularPostsCache();
+    }
+}
