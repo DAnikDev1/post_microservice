56c7c34600a372d24ac8d7a3629bbaf189fe8321
danikdev <danikdevs@gmail.com>
2025-07-08 20:36:56 +0500

    added system key support for other microservices

--- src/main/java/src/danik/postservice/config/user/UserHeaderFilter.java
+++ src/main/java/src/danik/postservice/config/user/UserHeaderFilter.java
@@ -3,6 +3,7 @@ package src.danik.postservice.config.user;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import src.danik.postservice.exception.NoAccessException;
 
@@ -12,16 +13,23 @@ import java.io.IOException;
 @Component
 public class UserHeaderFilter implements Filter {
     private final UserContext userContext;
+    @Value("${system.key}")
+    private String secretSystemKey;
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String userId = httpServletRequest.getHeader("social-user-id");
+        String systemKey = httpServletRequest.getHeader("system-key");
         if (userId != null && !userId.isEmpty()) {
             try {
                 userContext.setUserIdVault(Long.parseLong(userId));
             } catch (NumberFormatException e) {
-                throw new NoAccessException("Request without social user id");
+                if (systemKey != null && !systemKey.isEmpty() && systemKey.equals(secretSystemKey)) {
+                    filterChain.doFilter(servletRequest, servletResponse);
+                } else {
+                    throw new NoAccessException("Request without social user id");
+                }
             }
         }
         try {
